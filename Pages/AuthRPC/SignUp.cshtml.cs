eb9e6d88161ef1109f8012e69ac0a325a6e40ed9
JuanPalta <juanpalta24@hotmail.com>
2022-02-06 08:43:52 -0500

    feat: add validation password and creation of User

--- Pages/AuthRPC/SignUp.cshtml.cs
+++ Pages/AuthRPC/SignUp.cshtml.cs
@@ -4,17 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using rpc_razor_auth.Data;
 using rpc_razor_auth.Models;
 
 namespace rpc_razor_auth.Pages.AuthRPC
 {
     public class SignUpModel : PageModel
     {
+        private readonly UsersDbContext _db;
+        public SignUpModel(UsersDbContext db)
+        {
+            _db = db;
+        }
+
         [BindProperty]
         public User User { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost(string ValidationPassword)
+        {
+            if (!ModelState.IsValid || ValidationPassword != User.Password)
+            {
+                return Page();
+            }
+            _db.Add(User);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("SignIn");
         }
     }
 }
