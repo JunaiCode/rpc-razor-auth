e6d813250c8343e7c63fd604da9508d880bacd4c
JuanPalta <juanpalta24@hotmail.com>
2022-02-06 10:13:55 -0500

    feat: Sign in find user validation

--- Pages/AuthRPC/SignIn.cshtml.cs
+++ Pages/AuthRPC/SignIn.cshtml.cs
@@ -4,16 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using rpc_razor_auth.Data;
 using rpc_razor_auth.Models;
 
 namespace rpc_razor_auth.Pages.AuthRPC
 {
     public class IndexModel : PageModel
     {
+        private readonly UsersDbContext _db;
+        public IndexModel(UsersDbContext db)
+        {
+            _db = db;
+        }
+
+        
         [BindProperty]
-        public User User { get; set; }
+        public new User User { get; set; }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost(string pw, string us)
+        {
+            User = _db.User.Find(us);
+            if (User != null)
+            {
+                if (!User.Password.Equals(pw))
+                {
+                    return Page();
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToPage("SignUp");
+            }
+            return Page();
         }
     }
 }
