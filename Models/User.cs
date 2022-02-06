81e0c1ffdf9c846a7ea443248d275da1688a446e
JuanPalta <juanpalta24@hotmail.com>
2022-02-06 07:36:33 -0500

    feat: add Sign Up Design

--- Models/User.cs
+++ Models/User.cs
@@ -7,20 +7,28 @@ namespace rpc_razor_auth.Models
 	{
 		[Key]
 		[Required]
+		[Display(Name = "User Name")]
 		public String UserName { get; set; }
-
+		
 		[Required]
 		public String Password { get; set; }
 
 		[Required]
+		[Display(Name = "First Name")]
 		public String FirstName { get; set; }
 		
 		[Required]
+		[Display(Name = "Last Name")]
 		public String LastName { get; set; }
 
 		[DataType(DataType.Date)]
+		[Display(Name = "Birth Date")]
 		public DateTime BirthDate { get; set; }
 
+		public User()
+        {
+
+        }
 		public User(String userName, String password, String firstName, String lastName, DateTime birthDate)
 		{
 			this.UserName = userName;
