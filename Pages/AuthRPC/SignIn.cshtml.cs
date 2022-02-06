6ffb6ecca351df668b99155a241459863eef7785
JuanPalta <juanpalta24@hotmail.com>
2022-02-06 07:52:50 -0500

    feat: Add Sign In Design

--- Pages/AuthRPC/SignIn.cshtml.cs
+++ Pages/AuthRPC/SignIn.cshtml.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using rpc_razor_auth.Models;
 
 namespace rpc_razor_auth.Pages.AuthRPC
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public User User { get; set; }
         public void OnGet()
         {
         }
